87c04df2ded8c8f42d779eb60f7b6dc1bb26d7f2
Graham Joonsar <70826772+GrahamJoonsar@users.noreply.github.com>
2021-07-21 22:56:11 -0400

    Added initial files

---
+++ build.bat
@@ -0,0 +1,5 @@
+@echo off
+cls
+echo Starting Build
+g++ main.cpp -o main.exe
+echo Build Complete
---
+++ main.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+int main(void){
+    std::cout << "Time for tetris" << std::endl;
+}
